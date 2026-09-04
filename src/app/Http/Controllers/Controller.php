319b0588229dce0913e897089ac8599149f18523
giovanni.rojas@documenta.com.py <giovanni.rojas@documenta.com.py>
2026-09-04 15:54:50 -0300

    feat: implementar swagger-php y Swagger UI para documentacion del CRUD de reservas

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(
+        version: "1.0.0",
+        title: "Menina Coiffure API",
+        description: "Documentación interactiva de la API para el sistema de reservas y gestión de peluquería Menina Coiffure."
+    ),
+    OA\Server(
+        url: "http://localhost:8000",
+        description: "Servidor de Desarrollo Local"
+    ),
+    OA\Tag(
+        name: "Reservas",
+        description: "Operaciones y ciclo de vida del CRUD de Reservas"
+    )
+]
 abstract class Controller
 {
     //
 }
+
